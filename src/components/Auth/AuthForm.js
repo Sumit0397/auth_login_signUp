9cd2eefbb442406020168eb6b98c0b6133e8c065
Sumit0397 <sumitmajumdar16@gmail.com>
2023-08-15 11:46:29 +0530

    add auth context

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -1,12 +1,14 @@
-import { useState, useRef } from 'react';
+import { useState, useRef, useContext } from 'react';
 
 import classes from './AuthForm.module.css';
+import AuthContext from '../Store/auth-context';
 
 const AuthForm = () => {
 
   const emailInputRef = useRef();
   const passwordInputRef = useRef();
 
+  const authCtx = useContext(AuthContext);
   
   const [isLogin, setIsLogin] = useState(true);
   const [isLoading , setIsLoading] = useState(false);
@@ -56,7 +58,7 @@ const AuthForm = () => {
           })
         }
       }).then(data => {
-        console.log(data);
+        authCtx.logIn(data.idToken);
       }).catch((err) => {
         alert(err.message)
       })
---
+++ src/components/Store/auth-context.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+
+const AuthContext = React.createContext({
+    token : "",
+    isLoggedIn : false,
+    logIn : (token) => {},
+    logOut : () => {}
+})
+
+
+export const AuthContextProvider = (props) => {
+    const [token,setToken] = useState(null);
+
+    const userLoggedIn = !!token;
+
+    const logInHandler = (token) => {
+        setToken(token);
+    }
+
+    const logOutHandler = () => {
+        setToken(null);
+    }
+
+    const contextValue = {
+        token : token,
+        isLoggedIn : userLoggedIn,
+        logIn : logInHandler,
+        logOut : logOutHandler
+    }
+
+    return (
+        <AuthContext.Provider value={contextValue}>
+            {props.children}
+        </AuthContext.Provider>
+    )
+}
+
+
+export default AuthContext
